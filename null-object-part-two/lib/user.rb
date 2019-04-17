2cb65ea35fe0dd2615a96093d76e700cfbc5eddf
Ekuma Chidi <Bukki.arise@yahoo.co.uk>
2019-04-16 23:22:43 -0100

    add null object part two solution

--- null-object-part-two/lib/user.rb
+++ null-object-part-two/lib/user.rb
@@ -1,39 +1,38 @@
 class User
   include ActiveModel::Model
-  attr_accessor :created_at, :credit_card, :subscription
+  attr_accessor :created_at, :credit_card
+  attr_writer :subscription
 
   FREE_TRIAL = 'Free Trial'
   NO_PLAN = 'No Plan'
 
   def charge
-    unless subscription.nil?
-      subscription.charge(credit_card)
-    end
+    subscription.charge(credit_card)
   end
 
   def has_mentoring?
-    free_trial? || subscription && subscription.has_mentoring?
+    subscription.has_mentoring?
   end
 
   def price
-    if free_trial? || subscription.nil?
-      0
-    else
-      subscription.price
-    end
+    subscription.price
   end
 
   def plan_name
-    if free_trial?
-      FREE_TRIAL
-    elsif subscription
-      subscription.plan_name
-    else
-      NO_PLAN
-    end
+    subscription.plan_name
   end
 
   private
+
+  def subscription
+    if free_trial?
+      TrialSubscription.new
+    elsif @subscription.nil?
+      NoSubscription.new
+    else
+      @subscription
+    end
+  end
 
   def free_trial?
     created_at >= 30.days.ago
